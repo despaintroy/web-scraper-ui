1d4ca419e9b2233ef58491b211e57309dc972933
Troy DeSpain <despaintroy@gmail.com>
2024-09-08 12:38:24 -0600

    set up joyui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import '@fontsource/inter';
+import {CssVarsProvider} from "@mui/joy";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <CssVarsProvider>
+      <App/>
+    </CssVarsProvider>
   </StrictMode>,
 )
